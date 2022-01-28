bff4b895676f84154c65c89751e48fb21aa470b2
Sahil Batra <78542194+infotechprogrammer@users.noreply.github.com>
2022-01-28 15:40:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -24,8 +24,6 @@
     <!-- Theme color -->
     <link id="switcher" href="css/theme-color/default-theme.css" rel="stylesheet">
     <!-- <link id="switcher" href="css/theme-color/bridge-theme.css" rel="stylesheet"> -->
-    <!-- Top Slider CSS -->
-    <link href="css/sequence-theme.modern-slide-in.css" rel="stylesheet" media="all">
 
     <!-- Main style sheet -->
     <link href="css/style.css" rel="stylesheet">    
